722d360449bcdd05fd14b0744db0bf7868683620
Christian <cwooddel@u.rochester.edu>
2021-03-16 09:55:15 -0400

    create user and session reducers

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
+// import * as APIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
     ReactDOM.render(<Root store={store} />, root);
+
+    // window.signup = APIUtil.signup;
+    // window.login = APIUtil.login;
+    // window.logout = APIUtil.logout;
 });
