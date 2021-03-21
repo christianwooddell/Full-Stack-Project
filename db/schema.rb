b95cc54799b265878bbc5cf504b4e7d042fe2561
christian <christian.wooddell@rochester.edu>
2021-03-21 13:03:54 -0400

    add duration to songs and configure aws

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_000850) do
+ActiveRecord::Schema.define(version: 2021_03_21_170309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_000850) do
     t.integer "album_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "duration"
     t.index ["title"], name: "index_songs_on_title"
   end
 
