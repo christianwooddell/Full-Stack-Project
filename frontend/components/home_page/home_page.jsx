5381f32fcd0bcf39efe0a716b21b249ad11a5df7
christian <christian.wooddell@rochester.edu>
2021-03-22 16:51:02 -0400

    reconfigure music player

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import MusicPlayerNavBar from "../music_player/music_player_navbar";
-
+// import NowPlaying from "../now_playing/now_playing";
 
 const HomePage = ({ currentUser, logout }) => {
     const personalGreeting = () => (
@@ -12,11 +12,21 @@ const HomePage = ({ currentUser, logout }) => {
                 <MusicPlayerNavBar />
                 
                 <button className="header-button" onClick={logout}>Log Out</button>
-        
         </header>
+
     );
 
     return personalGreeting();
+
+    
+    // render () {
+        // return (
+        //     personalGreeting();
+    //         <div className="musiccontainer">  
+    //             <NowPlaying />
+    //         </div>
+        // )
+    // }
 }
 
 export default HomePage;
--- frontend/components/music_player/music_player.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MusicPlayerNavbar from './music_player_navbar_container';
-
-class MusicPlayer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.
-    }
-}
